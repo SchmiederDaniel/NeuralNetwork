d90df86641c924910d5b473d062f195909716e5b
SchmiederDaniel <145265483+SchmiederDaniel@users.noreply.github.com>
2025-09-08 11:45:24 +0200

    Addex Katex support and created a svelte element for it.

---
+++ src/lib/elements/Katex.svelte
@@ -0,0 +1,24 @@
+<script>
+	import katex from 'katex';
+	import { onMount } from 'svelte';
+
+	const { children } = $props();
+
+	let childrenElement = $state();
+	let renderKatex = $state();
+
+	onMount(() => {
+		let text = childrenElement.innerText;
+
+		renderKatex = katex.renderToString(text, {
+			displayMode: true,
+			throwOnError: false,
+			output: 'mathml'
+		});
+	});
+
+</script>
+<main>
+	<div class="hidden" bind:this={childrenElement}>{@render children()}</div>
+	{@html renderKatex}
+</main>
